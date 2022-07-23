ca985aa63e802e32de7837e22dce99870986a4b2
IkuoShige <85383887+IkuoShige@users.noreply.github.com>
2022-07-23 10:50:39 +0900

    Add files via upload

--- atcoder1.cpp
+++ atcoder1.cpp
@@ -3,9 +3,10 @@
 #include<iostream>
 #include<vector>
 int main(){
-    int h, w;
+    int h, w,sum;
     std::cin >> h >> w;
-    std::vector <std::string>A(h);
+    sum = h*w;
+    /*std::vector <std::string>A(h);
     for (int i = 0;i<w+2;i++){
         std::cout << "#";
     }
@@ -17,5 +18,34 @@ int main(){
     for (int i = 0;i<w+2;i++){
         std::cout << "#";
     }
+    std::cout << std::endl;*/
+    std::vector<std::vector<char>> A(h, std::vector<char>(w));
+    for (int i = 0;i<h;i++){
+        for (int j = 0;j<w;j++){
+            std::cin >> A.at(i).at(j);
+        }
+    }
+    //output
+    for (int i=0;i<w+2;i++){
+        std::cout << "#";
+    }
     std::cout << std::endl;
+    for (int i=0;i<h;i++){
+        std::cout << "#";
+        for (int j=0;j<w;j++){
+            std::cout << A.at(i).at(j);
+            if (j+1==w){
+                std::cout << "#" << std::endl;
+            }
+        }
+    }
+    for (int i=0;i<w+2;i++){
+        std::cout << "#";
+    }
+    std::cout << std::endl;
+    /*for (int i = 0;i<h;i++){//output
+        for (int j = 0;j<w;j++){
+             std::cout << A.at(i).at(j) << std::endl;
+         }
+    }*/
 }
